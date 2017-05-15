bda392dbd652c85e6643b753c8fa0af62c2b4d4a
Carlos Galdamez <Carlos Galdamez>
2017-05-15 10:44:18 -0700

    initial commit

---
+++ src/metrics/TransponderMetric.java
@@ -0,0 +1,69 @@
+package metrics;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import models.Pair;
+import simulator.Simulator;
+import utilities.NetworkTopology;
+
+public class TransponderMetric{
+	
+	private static enum BANDWIDTH_DISTRIBUTION { RANDOM, GAUSSIAN, UNIFORM };
+	private static enum EMBEDDING_METHOD { BACKUP, WO_BACKUP }; 
+	
+	private final String PROJECT_DIRECTORY =  new File(".").getCanonicalPath();
+	private final String RESULTS_DIRECTORY = "/src/results/";
+	
+	public TransponderMetric()throws IOException{}
+	
+	public void start() throws IOException{
+		getResults(BANDWIDTH_DISTRIBUTION.RANDOM,EMBEDDING_METHOD.WO_BACKUP);
+		getResults(BANDWIDTH_DISTRIBUTION.UNIFORM,EMBEDDING_METHOD.WO_BACKUP);
+		getResults(BANDWIDTH_DISTRIBUTION.GAUSSIAN,EMBEDDING_METHOD.WO_BACKUP);
+		
+		getResults(BANDWIDTH_DISTRIBUTION.RANDOM,EMBEDDING_METHOD.BACKUP);
+		getResults(BANDWIDTH_DISTRIBUTION.UNIFORM,EMBEDDING_METHOD.BACKUP);
+		getResults(BANDWIDTH_DISTRIBUTION.GAUSSIAN,EMBEDDING_METHOD.BACKUP);
+
+	}
+	
+	public void getResults(BANDWIDTH_DISTRIBUTION distributionType, EMBEDDING_METHOD method) throws IOException{
+		File file = new File(PROJECT_DIRECTORY + RESULTS_DIRECTORY + "transponder_correct_4node_"+ distributionType.name().toLowerCase() + "_" + method.name().toLowerCase() + ".csv");
+		PrintWriter pw = new PrintWriter(file);
+		
+		pw.println("Max_Bandwidth,ODU,OTN,# ODU better than OTN ");
+
+		for(int i = 10; i <= 200; i+=10){
+			System.out.println("Starting Transponder Metric with max bandwidth: " + i);
+			int oduWins = 0;
+			
+			int sum1 = 0;
+			int sum2 = 0;
+			for(int j = 0; j < 1000; j++){
+				Simulator simulator = new Simulator(NetworkTopology.SIMPLE,Integer.MAX_VALUE, Integer.MAX_VALUE);
+				simulator.setNumberOfRequest(500);
+				simulator.generateRequests();
+				
+				int oduTransponders = simulator.getTranspondersODU(100,i, distributionType.name().toLowerCase(), (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true);
+				int otnTransponders = simulator.getTransponderOTN(100,i, distributionType.name().toLowerCase(), (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true);
+				
+				if(oduTransponders < otnTransponders) oduWins++;
+				
+				sum1 += oduTransponders; 
+				sum2 += otnTransponders;
+			}
+			Pair<Integer,Integer> results = new Pair<Integer, Integer>(sum1/1000,sum2/1000);			
+			pw.println(i + "," + results.first() + "," + results.second() + "," + oduWins);
+		}
+		
+		pw.close();
+		
+		System.out.println("********************** done *******************");
+	}
+	
+	public static void main(String args[]) throws IOException{
+		new TransponderMetric().start();
+	}
+}
---
+++ src/models/Path.java
@@ -0,0 +1,126 @@
+package models;
+
+public class Path {
+
+	private int pathDistance;
+	private int throughput;
+	
+	private PathNode start;
+	
+	public Path(int originNodeID){
+		start = new PathNode(originNodeID);
+	}
+	
+	public int getThroughput(){
+		return throughput;
+	}
+	
+	public void setThroughput( int throughput){
+		this.throughput = throughput;
+	}
+	
+	public int getPathDistance(){
+		return pathDistance;
+	}
+	
+	public void setPathDistance(int pathDistance){
+		this.pathDistance = pathDistance;
+	}
+	
+	public PathNode getStart(){
+		return start;
+	}
+	
+	public PathNode getNode(int i){
+		PathNode current = start;
+		
+		int index = 0;
+		while(index != i){
+			current = current.next();
+			index++;
+		}
+		
+		return current;
+	}
+	
+	public boolean equals(Path p){
+		PathNode current = start;
+		PathNode currentCompare = p.getStart();
+		
+		if(this.getNumberOfHops() != p.getNumberOfHops())
+			return false;
+		
+		while(current.hasNext() && currentCompare.hasNext()){
+			if(current.getNodeID() != currentCompare.getNodeID())
+				return false;
+			current = current.next();
+			currentCompare = currentCompare.next();
+		}
+		
+		return true;
+	}
+	
+	public int getNumberOfHops(){
+		PathNode current = start;
+		
+		int hop = 0;
+		while(current.hasNext()){
+			current = current.next();
+			hop++;
+		}
+		
+		return hop;
+	}
+	
+	public Path getNodes(int e){
+		Path subPath = new Path(start.getNodeID());
+		if(e == 0)
+			return subPath;
+		
+		PathNode subCurrent = subPath.getStart();
+		PathNode current = start.next();
+		
+		int index = 1;
+		while(index != e + 1){
+			subCurrent.setNext(new PathNode(current.getNodeID()));
+			subCurrent = subCurrent.next();
+			current = current.next();
+			index++;
+		}
+		
+		return subPath;
+		
+	}
+	
+	public void append(Path path){
+		PathNode current = start;
+		
+		while(current.hasNext())
+			current = current.next();
+		
+		if(current.getNodeID() == path.getStart().getNodeID())
+			current.setNext(path.getStart().next());
+		else
+			current.setNext(path.getStart());
+		
+		pathDistance += path.getPathDistance();
+	}
+	
+	public String toString(){
+		StringBuilder sb = new StringBuilder("PATH: ");
+		
+		PathNode current = start;
+		
+		do{
+			sb.append(current.getNodeID() + ((current.hasNext())? " --> " : ""));
+			current = current.next();
+		}while (current != null);
+		
+		sb.append("\nPath Distance: " + pathDistance);
+		//sb.append("\nThroughput: " + throughput + "\n");
+		
+		return sb.toString();
+	}
+	
+	
+}
